3b4b525eea4214c2faddd965168754f2cd66a0ef
Jack Gerrits <jackgerrits@users.noreply.github.com>
2021-04-18 18:32:18 -0400

    Update benchmarks_common.h

--- test/benchmarks/benchmarks_common.h
+++ test/benchmarks/benchmarks_common.h
@@ -3,21 +3,21 @@
 #include <sstream>
 #include <string>
 
-inline std::string get_x_numerical_fts(int feature_size) {
+static inline std::string get_x_numerical_fts(int feature_size) {
   std::stringstream ss;
   ss << "1:1:0.5 |";
   for (size_t i = 0; i < feature_size; i++) { ss << " " << std::to_string(i) + ":4.36352"; }
   return ss.str();
 };
 
-inline std::string get_x_string_fts(int feature_size) {
+static inline std::string get_x_string_fts(int feature_size) {
   std::stringstream ss;
   ss << "1:1:0.5 | ";
   for (size_t i = 0; i < feature_size; i++) { ss << "bigfeaturename" + std::to_string(i) + ":10 "; }
   return ss.str();
 };
 
-inline std::string get_x_string_fts_no_label(int feature_size, size_t action_index = 0) {
+static inline std::string get_x_string_fts_no_label(int feature_size, size_t action_index = 0) {
   std::stringstream ss;
   ss << " | ";
   for (size_t j = 0; j < feature_size; j++) { ss << std::to_string(action_index) + "_" + std::to_string(j) << +" "; }
@@ -26,7 +26,7 @@ inline std::string get_x_string_fts_no_label(int feature_size, size_t action_ind
   return ss.str();
 };
 
-inline std::string get_x_string_fts_multi_ex(int feature_size, size_t actions, bool shared, bool label, size_t start_index = 0) {
+static inline std::string get_x_string_fts_multi_ex(int feature_size, size_t actions, bool shared, bool label, size_t start_index = 0) {
   size_t action_start = 0;
   std::stringstream ss;
   if (shared) { ss << "shared | s_1 s_2 s_3 s_4" << std::endl; }
