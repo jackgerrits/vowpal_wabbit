3e12c87c69b43de17703224cedcecb1279ce7a9f
Jack Gerrits <jack@jackgerrits.com>
2021-04-17 22:30:28 -0400

    dont treat warnings as errors fix inlines

--- test/benchmarks/benchmarks_common.h
+++ test/benchmarks/benchmarks_common.h
@@ -2,23 +2,21 @@
 #include <sstream>
 #include <string>
 
-auto get_x_numerical_fts = [](int feature_size) {
+inline std::string get_x_numerical_fts(int feature_size) {
   std::stringstream ss;
   ss << "1:1:0.5 |";
   for (size_t i = 0; i < feature_size; i++) { ss << " " << std::to_string(i) + ":4.36352"; }
-  std::string s = ss.str();
-  return s;
+  return ss.str();
 };
 
-auto get_x_string_fts = [](int feature_size) {
+inline std::string get_x_string_fts(int feature_size) {
   std::stringstream ss;
   ss << "1:1:0.5 | ";
   for (size_t i = 0; i < feature_size; i++) { ss << "bigfeaturename" + std::to_string(i) + ":10 "; }
-  std::string s = ss.str();
-  return s;
+  return ss.str();
 };
 
-auto get_x_string_fts_no_label = [](int feature_size, size_t action_index = 0) {
+inline std::string get_x_string_fts_no_label(int feature_size, size_t action_index = 0) {
   std::stringstream ss;
   ss << " | ";
   for (size_t j = 0; j < feature_size; j++) { ss << std::to_string(action_index) + "_" + std::to_string(j) << +" "; }
@@ -27,7 +25,7 @@ auto get_x_string_fts_no_label = [](int feature_size, size_t action_index = 0) {
   return ss.str();
 };
 
-auto get_x_string_fts_multi_ex = [](int feature_size, size_t actions, bool shared, bool label, size_t start_index = 0) {
+inline std::string get_x_string_fts_multi_ex(int feature_size, size_t actions, bool shared, bool label, size_t start_index = 0) {
   size_t action_start = 0;
   std::stringstream ss;
   if (shared) { ss << "shared | s_1 s_2 s_3 s_4" << std::endl; }
