e42d31ca9afd93ff712676ce7a92a1c7f15e95f1
Griffin Bassman <griffinbassman@gmail.com>
2021-07-13 16:30:18 -0400

    test

--- vowpalwabbit/slim/src/vw_slim_predict.cc
+++ vowpalwabbit/slim/src/vw_slim_predict.cc
@@ -16,7 +16,7 @@ uint64_t ceil_log_2(uint64_t v)
 namespace_copy_guard::namespace_copy_guard(example_predict& ex, uint64_t ns, unsigned const char index)
     : _ex(ex), _ns(ns)
 {
-  _ex.feature_space.get_or_create(index, _ns);
+  _ex.feature_space.get_or_create(index, _ns); 
 }
 
 namespace_copy_guard::~namespace_copy_guard() { _ex.feature_space.remove(_ns); }
